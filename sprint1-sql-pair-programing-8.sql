69aff8fb9c10989dd28bcf747f54b8f5fb066b40
Silvia-Ag <alfayate.silvia@hotmail.com>
2023-02-28 21:41:21 +0100

    pair sql terminado

--- sprint1-sql-pair-programing-8.sql
+++ sprint1-sql-pair-programing-8.sql
@@ -1,66 +1,33 @@
- CREATE SCHEMA `tienda_zapatillas`;
- USE `tienda_zapatillas`;
- 
- CREATE TABLE `Zapatillas`(
- `id_zapatilla` INT NOT NULL AUTO_INCREMENT,
- `modelo` VARCHAR(45) NOT NULL,
- `color` VARCHAR(45) NOT NULL,
- PRIMARY KEY(`id_zapatilla`)
- );
-    
-CREATE TABLE `Clientes`(
-	`id_cliente` INT NOT NULL AUTO_INCREMENT,
-    `nombre` VARCHAR(45) NOT NULL,
-    `numero_telefono` INT(9) NOT NULL,
-    `email` VARCHAR(45) NOT NULL,
-    `direccion` VARCHAR(45) NOT NULL,
-    `ciudad` VARCHAR(45) NOT NULL,
-    `provincia` VARCHAR(45) NOT NULL,
-    `pais` VARCHAR(45) NOT NULL,
-    `codigo_postal` VARCHAR(45) NOT NULL,    
-    PRIMARY KEY(`id_cliente`)
-);
-    
-CREATE TABLE `Empleados`(
-	`id_empleado` INT NOT NULL AUTO_INCREMENT,
-    `nombre` VARCHAR(45) NOT NULL,
-    `tienda` VARCHAR(45) NOT NULL,
-    `salario` INT NOT NULL,
-    `fecha_incorporacion` DATE NOT NULL,
-    PRIMARY KEY(`id_empleado`)
-);
+USE `tienda_zapatillas`;
 
-CREATE TABLE `Facturas`(
-	`id_factura` INT NOT NULL AUTO_INCREMENT,
-    `numero_factura` VARCHAR(45) NOT NULL,
-    `fecha` DATE NOT NULL,
-    `id_zapatilla` INT NOT NULL,
-    `id_empleado` INT NOT NULL,
-    `id_cliente` INT NOT NULL,
-    PRIMARY KEY(`id_factura`),
-    CONSTRAINT `fk_Facturas_Zapatillas`
-		FOREIGN KEY(`id_zapatilla`)
-        REFERENCES `Zapatillas` (`id_zapatilla`) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT `fk_Facturas_Empleados`
-		FOREIGN KEY(`id_empleado`)
-		REFERENCES `Empleados` (`id_empleado`) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT `fk_Facturas_Clientes`
-		FOREIGN KEY(`id_cliente`)
-        REFERENCES `Clientes` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE
-);
-      
-ALTER TABLE `Zapatillas`
-ADD COLUMN `marca` VARCHAR(45),
-ADD COLUMN `talla` INT NOT NULL;
+INSERT INTO `zapatillas` (`modelo`, `color`, `marca`, `talla`)
+		VALUES ( 'XQYUN', 'Negro','Nike', 42), ('UOPMN', 'Rosas', 'Nike', 39), ('OPNYT', 'Verdes', 'Adidas', 35);
 
-ALTER TABLE `Empleados`
-MODIFY `salario` FLOAT;
+INSERT INTO `empleados` (`nombre`, `tienda`, `salario`, `fecha_incorporacion`)
+		VALUES ('Laura', 'Alcobendas', 25987, '2010-09-03'), ('Maria', 'Sevilla', NULL, '2001-04-11'), 
+				('Ester', 'Oviedo', 30165.98, '2000-11-29');
+        
+        
+INSERT INTO `clientes`(`nombre`, `numero_telefono`, `email`, `direccion`, `ciudad`, `provincia`, `codigo_postal`)
+	VALUES ('Monica', 1234567289, 'monica@email.com', 'Calle Felicidad', 'Móstoles', 'Madrid', 28176), 
+			('Lorena', 289345678, 'lorena@email.com', 'Calle Alegria', 'Barcelona', 'Barcelona', 12346),
+			('Carmen', 298463759, 'carmen@email.com', 'Calle del Color', 'Vigo', 'Pontevedra', 23456);
+            
+INSERT INTO `facturas`(`numero_factura`, `fecha`, `id_zapatilla`, `id_empleado`, `id_cliente`, `total`)
+	VALUES (123, '2001-12-11', 1, 2, 1, 54.98), (1234, '2005-05-23', 1, 1, 3, 89.91), (12345, '2015-09-18', 2, 3, 3, 76.23);
 
-ALTER TABLE `Clientes`
-DROP COLUMN `pais`;
+UPDATE `zapatillas`
+SET `color` = 'Amarillas'
+WHERE `id_zapatilla` = 2;
 
-ALTER TABLE `Clientes`
-MODIFY `codigo_postal` INT(5) ZEROFILL;
+UPDATE `empleados`
+SET `tienda` = 'A Coruña'
+WHERE `id_empleado` = 1;
 
-ALTER TABLE `Facturas`
-ADD COLUMN `total` FLOAT;
+UPDATE `clientes`
+SET `numero_telefono` = 123456728
+WHERE `id_cliente` = 1;
+
+UPDATE `facturas`
+SET `total` = 89.91
+WHERE `id_factura` = 2;
